a5f216db34031c4df3019615ff81cf95e652635a
Uche Joseph <josephuche2404@gmail.com>
2025-05-23 09:45:21 +0300

    Built appplication and copied maximum.txt file to the build folder

---
+++ build/CMakeFiles/bankers_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bankers_algorithm.dir/banker.cpp.obj"
+  "CMakeFiles/bankers_algorithm.dir/banker.cpp.obj.d"
+  "CMakeFiles/bankers_algorithm.dir/main.cpp.obj"
+  "CMakeFiles/bankers_algorithm.dir/main.cpp.obj.d"
+  "bankers_algorithm.exe"
+  "bankers_algorithm.exe.manifest"
+  "bankers_algorithm.pdb"
+  "libbankers_algorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bankers_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
